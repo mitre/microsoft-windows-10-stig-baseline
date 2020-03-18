b915d5a36aa858a0fcc4afab6749e09c11576e8d
Aaron Lippold <lippold@gmail.com>
2020-03-18 09:47:41 -0400

    First batch of fixes - updated formatting and layout - updated implementation of many of the NA controls - fixed basic errors and missing end statments - standardized the logic flow of many of the controls
    Notes:
    
    - We should add a ReleaseID propertiy to one of the windows resources
    - Need to add the ability in only_if to update the impact
    - Most of the unclassified controls could be implemented in one control that uses an input
      which is a hash perhaps?
    - V-74725 - The NA case is not accounted for - and its somewhat insane need to talk to DISA
    - v-74415 - does this mean it is na for 1507 and 1607 like the control after it ? etc?
    
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/V-74415.rb
+++ controls/V-74415.rb
@@ -1,4 +1,6 @@
-control "V-74415" do
+# frozen_string_literal: true
+
+control 'V-74415' do
   title "Windows 10 must be configured to prevent Microsoft Edge browser data
 from being cleared on exit."
   desc  "Clearing browser data on exit automatically deletes specified items
@@ -7,14 +9,14 @@ malicious websites and files that could later be used for anti-virus and
 Intrusion Detection System (IDS) signatures.  Disabling this function will
 prevent the data from automatically being deleted when the browser closes."
   impact 0.5
-  tag severity: "medium"
-  tag gtitle: "WN10-CC-000228"
-  tag gid: "V-74415"
-  tag rid: "SV-89089r4_rule"
-  tag stig_id: "WN10-CC-000228"
-  tag fix_id: "F-80957r1_fix"
-  tag cci: ["CCI-000366"]
-  tag nist: ["CM-6 b", "Rev_4"]
+  tag severity: 'medium'
+  tag gtitle: 'WN10-CC-000228'
+  tag gid: 'V-74415'
+  tag rid: 'SV-89089r4_rule'
+  tag stig_id: 'WN10-CC-000228'
+  tag fix_id: 'F-80957r1_fix'
+  tag cci: ['CCI-000366']
+  tag nist: ['CM-6 b', 'Rev_4']
   tag false_negatives: nil
   tag false_positives: nil
   tag documentable: false
@@ -26,25 +28,31 @@ prevent the data from automatically being deleted when the browser closes."
   tag responsibility: nil
   tag ia_controls: nil
   tag check: "Windows 10 LTSC\\B versions do not include Microsoft Edge, this
-is NA for those systems.
+      is NA for those systems.
 
-If the following registry value does not exist or is not configured as
-specified, this is a finding.
+      If the following registry value does not exist or is not configured as
+      specified, this is a finding.
 
-Registry Hive: HKEY_LOCAL_MACHINE
-Registry Path: \\SOFTWARE\\Policies\\Microsoft\\MicrosoftEdge\\Privacy\\
+      Registry Hive: HKEY_LOCAL_MACHINE
+      Registry Path: \\SOFTWARE\\Policies\\Microsoft\\MicrosoftEdge\\Privacy\\
 
-Value Name: ClearBrowsingHistoryOnExit
+      Value Name: ClearBrowsingHistoryOnExit
 
-Type: REG_DWORD
-Value: 0x00000000 (0)"
+      Type: REG_DWORD
+      Value: 0x00000000 (0)"
   tag fix: "Configure the policy value for Computer Configuration >>
-Administrative Templates >> Windows Components >> Microsoft Edge >> \"Allow
-clearing browsing data on exit\" to \"Disabled\"."
+      Administrative Templates >> Windows Components >> Microsoft Edge >> \"Allow
+      clearing browsing data on exit\" to \"Disabled\"."
 
-  describe registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Privacy') do
-    it { should have_property 'ClearBrowsingHistoryOnExit' }
-    its('ClearBrowsingHistoryOnExit') { should cmp 0 }
+  if releaseID.match?(1607 | 1507) || releaseID < 1507
+    impact 0.0
+    describe 'This STIG does not apply to Prior Versions before 1507 and 1607.' do
+      skip 'This STIG does not apply to Prior Versions before 1507 and 1607.'
+    end
+  else
+    describe registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Privacy') do
+      it { should have_property 'ClearBrowsingHistoryOnExit' }
+      its('ClearBrowsingHistoryOnExit') { should cmp 0 }
+    end
   end
 end
-
--- controls/V-74719.rb
+++ controls/V-74719.rb
@@ -1,18 +1,20 @@
-control "V-74719" do
-  title "The Secondary Logon service must be disabled on Windows 10."
+# frozen_string_literal: true
+
+control 'V-74719' do
+  title 'The Secondary Logon service must be disabled on Windows 10.'
   desc  "The Secondary Logon service provides a means for entering alternate
-credentials, typically used to run commands with elevated privileges.  Using
-privileged credentials in a standard user session can expose those credentials
-to theft."
+        credentials, typically used to run commands with elevated privileges.  Using
+        privileged credentials in a standard user session can expose those credentials
+        to theft."
   impact 0.5
-  tag severity: "medium"
-  tag gtitle: "WN10-00-000175"
-  tag gid: "V-74719"
-  tag rid: "SV-89393r2_rule"
-  tag stig_id: "WN10-00-000175"
-  tag fix_id: "F-81333r1_fix"
-  tag cci: ["CCI-000381"]
-  tag nist: ["CM-7 a", "Rev_4"]
+  tag severity: 'medium'
+  tag gtitle: 'WN10-00-000175'
+  tag gid: 'V-74719'
+  tag rid: 'SV-89393r2_rule'
+  tag stig_id: 'WN10-00-000175'
+  tag fix_id: 'F-81333r1_fix'
+  tag cci: ['CCI-000381']
+  tag nist: ['CM-7 a', 'Rev_4']
   tag false_negatives: nil
   tag false_positives: nil
   tag documentable: false
@@ -25,20 +27,18 @@ to theft."
   tag ia_controls: nil
   tag check: "Run \"Services.msc\".
 
-Locate the \"Secondary Logon\" service.
+      Locate the \"Secondary Logon\" service.
 
-If the \"Startup Type\" is not \"Disabled\" or the \"Status\" is \"Running\",
-this is a finding."
-  tag fix: "Configure the \"Secondary Logon\" service \"Startup Type\" to
-\"Disabled\"."
+      If the \"Startup Type\" is not \"Disabled\" or the \"Status\" is \"Running\",
+      this is a finding."
+  tag fix: 'Configure the "Secondary Logon" service "Startup Type" to "Disabled".'
 
   describe.one do
     describe service('Secondary Logon') do
       it { should_not be_enabled }
     end
     describe service('Secondary Logon') do
-      it { should_not be_running}
+      it { should_not be_running }
     end
   end
 end
-
