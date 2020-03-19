a14af77c4be10dc7c62c330e3033d1d4b1ddc5c0
Jared Burns <burns.jared@gmail.com>
2020-03-19 12:14:59 -0400

    Idenified issues with values
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63861.rb
+++ controls/V-63861.rb
@@ -49,25 +49,7 @@ LOCAL SERVICE
 NETWORK SERVICE
 SERVICE"
 
-  describe.one do
     describe security_policy do
-      its('SeCreateGlobalPrivilege') { should eq ['S-1-5-32-544', 'S-1-5-19', 'S-1-5-20', 'S-1-5-6'] }
+      its('SeCreateGlobalPrivilege') { should include "['S-1-5-32-544', 'S-1-5-19', 'S-1-5-20', 'S-1-5-6']"}
     end
-    describe security_policy do
-      its('SeCreateGlobalPrivilege') { should eq ['S-1-5-32-544'] }
-    end
-    describe security_policy do
-      its('SeCreateGlobalPrivilege') { should eq ['S-1-5-19'] }
-    end
-    describe security_policy do
-      its('SeCreateGlobalPrivilege') { should eq ['S-1-5-20'] }
-    end
-    describe security_policy do
-      its('SeCreateGlobalPrivilege') { should eq ['S-1-5-6'] }
-    end
-    describe security_policy do
-      its('SeCreateGlobalPrivilege') { should eq [] }
-    end
-  end
 end
-
