633697d0672b6ecc528a82fd0743b57a3dc274ef
Jared Burns <burns.jared@gmail.com>
2020-03-19 14:12:17 -0400

    Updated its statement to not have include and have be_in
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63877.rb
+++ controls/V-63877.rb
@@ -75,23 +75,14 @@ Guests Group"
 is_domain = command('wmic computersystem get domain | FINDSTR /V Domain').stdout.strip
 
   if is_domain == 'WORKGROUP'
-    describe.one do
       describe security_policy do
         its('SeDenyInteractiveLogonRight') { should eq ['S-1-5-32-546'] }
       end
-      describe security_policy do
-        its('SeDenyInteractiveLogonRight') { should eq [] }
-      end
-    end
-
   else
     get_domain_sid = command('wmic useraccount get sid | FINDSTR /V SID | Select -First 2').stdout.strip
     domain_sid = get_domain_sid[9..40]
     describe security_policy do
-      its('SeDenyInteractiveLogonRight') { should include "S-1-21-#{domain_sid}-512" }
-    end
-    describe security_policy do
-      its('SeDenyInteractiveLogonRight') { should include "S-1-21-#{domain_sid}-519" }
+      its('SeDenyInteractiveLogonRight') { should be_in "S-1-21-#{domain_sid}512, S-1-21-#{domain_sid}519" }
     end
   end
 end
