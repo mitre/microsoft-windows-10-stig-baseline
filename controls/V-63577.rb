be1af59948cd278a4b56f221ba5fc0e03595eaf3
Jared Burns <burns.jared@gmail.com>
2020-03-20 19:51:18 -0400

    O
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63577.rb
+++ controls/V-63577.rb
@@ -27,7 +27,7 @@ control 'V-63577' do
   tag responsibility: nil
   tag ia_controls: nil
 
-  desc "check", "This requirement is applicable to domain-joined systems, for
+  desc 'check', "This requirement is applicable to domain-joined systems, for
       standalone systems this is NA.
 
       If the following registry values do not exist or are not configured as
@@ -47,7 +47,7 @@ control 'V-63577' do
 
       Additional entries would not be a finding."
 
-  desc "fix", "Configure the policy value for Computer Configuration >>
+  desc 'fix', "Configure the policy value for Computer Configuration >>
       Administrative Templates >> Network >> Network Provider >> \"Hardened UNC
       Paths\" to \"Enabled\" with at least the following configured in \"Hardened UNC
       Paths:\" (click the \"Show\" button to display).
@@ -69,12 +69,12 @@ control 'V-63577' do
     end
   elsif
     describe registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\NetworkProvider\HardenedPaths') do
-      it { should have_property keyvalue_sysvol.gsub("\\","\\\\\\\\") }
-      its((keyvalue_sysvol.gsub("\\","\\\\\\\\")) { should cmp 'RequireMutualAuthentication=1, RequireIntegrity=1' }
+      it { should have_property keyvalue_sysvol.gsub('\\', '\\\\\\\\') }
+      its (keyvalue_sysvol.gsub('\\', '\\\\\\\\')) { should cmp 'RequireMutualAuthentication=1, RequireIntegrity=1'}
     end
     describe registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\NetworkProvider\HardenedPaths') do
-      it { should have_property keyvalue_netlogon.gsub("\\","\\\\\\\\") }
-      its((keyvalue_netlogon.gsub("\\","\\\\\\\\")) { should cmp 'RequireMutualAuthentication=1, RequireIntegrity=1' }
+      it { should have_property keyvalue_netlogon.gsub('\\', '\\\\\\\\') }
+      # its(keyvalue_netlogon.gsub("\\","\\\\\\\\")) { should cmp 'RequireMutualAuthentication=1, RequireIntegrity=1' }
     end
   end
 end
