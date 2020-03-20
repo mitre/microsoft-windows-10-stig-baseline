7cca07dd47aa94cf87d594d09052ff38b3003469
Jared Burns <burns.jared@gmail.com>
2020-03-20 11:04:19 -0400

    P
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63871.rb
+++ controls/V-63871.rb
@@ -99,7 +99,8 @@ control 'V-63871' do
     get_domain_sid = command('wmic group where name="Domain Users" get sid').stdout.strip
     domain_sid = get_domain_sid[9..40]
     describe security_policy do
-      its('SeDenyNetworkLogonRight') { should cmp ["S-1-5-21-#{domain_sid}519"] }
+      its('SeDenyNetworkLogonRight') { should eq "*S-1-5-21-" }
+      #its('SeDenyNetworkLogonRight') { should eq "S-1-5-21-#{domain_sid}519" }
     end
     describe security_policy do
       its('SeDenyNetworkLogonRight') { should cmp "S-1-5-21-#{domain_sid}512" }
