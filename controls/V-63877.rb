c7ef4995eab9ef08efab13fe8daf18d40e81839d
Jared Burns <burns.jared@gmail.com>
2020-03-20 16:17:41 -0400

    removed describe.one
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63877.rb
+++ controls/V-63877.rb
@@ -77,14 +77,9 @@ control 'V-63877' do
   is_domain = command('wmic computersystem get domain | FINDSTR /V Domain').stdout.strip
 
   if is_domain == 'WORKGROUP'
-    describe.one do
       describe security_policy do
         its('SeDenyInteractiveLogonRight') { should eq ['S-1-5-32-546'] }
       end
-      describe security_policy do
-        its('SeDenyInteractiveLogonRight') { should eq [] }
-      end
-    end
   else
     get_domain_sid = command('wmic useraccount get sid | FINDSTR /V SID | Select -First 2').stdout.strip
     domain_sid = get_domain_sid[9..40]
