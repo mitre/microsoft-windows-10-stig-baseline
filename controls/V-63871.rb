50f0afa2676ade030f0c72429d54725e93213c14
Jared Burns <burns.jared@gmail.com>
2020-03-20 16:34:55 -0400

    updated
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63871.rb
+++ controls/V-63871.rb
@@ -91,7 +91,7 @@ control 'V-63871' do
   script = <<-EOH
         $get_domain_sid = wmic group get Name,SID | FINDSTR /C:"Domain Users
         $domain_sid = $get_domain_sid[50..79]
-        write-output $domain_sid.stdout
+        write-output $domain_sid
         EOH
 
   is_domain = command('wmic computersystem get domain | FINDSTR /V Domain').stdout.strip
