40c678d9ac5254c6ff72f23e1bac5eae7460a10a
Jared Burns <burns.jared@gmail.com>
2020-03-23 14:28:33 -0400

    Updated Its statement
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63851.rb
+++ controls/V-63851.rb
@@ -46,6 +46,6 @@ control 'V-63851' do
       Users"
 
     describe security_policy do
-      its('SeInteractiveLogonRight') { should eq ['S-1-5-32-544', 'S-1-5-32-545'] }
+      its('SeInteractiveLogonRight') { should be_in ['S-1-5-32-544', 'S-1-5-32-545'] }
     end
 end
