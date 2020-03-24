205a64a5ea359adbcba3442447511322bcd27b31
Jared Burns <burns.jared@gmail.com>
2020-03-24 16:42:19 -0400

    Updated Describe
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-63871.rb
+++ controls/V-63871.rb
@@ -101,10 +101,10 @@ control 'V-63871' do
     domain_sid = input('domain_sid')
     
     describe security_policy do
-      its('SeDenyNetworkLogonRight') { should include "S-1-5-21-#{domain_sid}-519" }
+      its('SeDenyNetworkLogonRight') { should include "S-1-5-21-#{domain_sid}-519. S-1-5-21-#{domain_sid}-512" }
     end
-    describe security_policy do
-      its('SeDenyNetworkLogonRight') { should include "S-1-5-21-#{domain_sid}-512" }
-    end
+    #describe security_policy do
+     # its('SeDenyNetworkLogonRight') { should include "S-1-5-21-#{domain_sid}-512" }
+    #end
   end
 end
